24dc55b5166227cf94befd386c6e63330c157f58
zhangzhen <1094705507@qq.com>
2019-07-28 17:49:38 +0800

    删除不必要文件

--- images.js
+++
@@ -1,5 +0,0 @@
-const images = require("images");
-images("./2.jpg")
-	.save("output.jpg", {               //Save the image to a file, with the quality of 50
-		quality : 20                    //保存图片到文件,图片质量为50
-	});
